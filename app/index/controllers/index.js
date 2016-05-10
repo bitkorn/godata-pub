37e5e11e3d36a651c539ed20cba7cb1f6452b2ed
Torsten Brieskorn <mail@bitkorn.de>
2016-05-10 12:43:46 +0200

    login AND logout

--- app/index/controllers/index.js
+++ app/index/controllers/index.js
@@ -14,13 +14,40 @@ godataAppIndexControllers.controller('IndexCtrl', ['$scope', '$rootScope', 'base
             $http.get(restDomain + '/auth', null, {})
                     .then(function (result) {
 //                        console.log('IndexCtrl: ' + JSON.stringify(result));
-                        if(result.data.result === 1) {
+                        if (result.data.result === 1) {
                             $cookies.put('auth', credentialString);
                             $rootScope.alerts = [{type: 'success', msg: 'access granted'}];
+                            $rootScope.access = true;
                         }
                     })
                     .catch(function (result) {
+                        $cookies.remove('auth');
+                        $http.defaults.headers.common['Authorization'] = '';
+                        $rootScope.access = false;
                         console.log(JSON.stringify(result));
                     });
+        };
+
+        var checkLogin = function () {
+            $http.get(restDomain + '/auth', null, {})
+                    .then(function (result) {
+//                        console.log('IndexCtrl: ' + JSON.stringify(result));
+                        if (result.data.result === 1) {
+                            $rootScope.access = true;
+                        }
+                    })
+                    .catch(function (result) {
+                        $cookies.remove('auth');
+                        $http.defaults.headers.common['Authorization'] = '';
+                        $rootScope.access = false;
+                        console.log(JSON.stringify(result));
+                    });
+        };
+        checkLogin();
+
+        $scope.godataLogout = function () {
+            $cookies.remove('auth');
+            $http.defaults.headers.common['Authorization'] = '';
+            $rootScope.access = false;
         };
     }]);
